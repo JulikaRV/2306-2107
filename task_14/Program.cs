3dc70f69e1ab21e9509588468398d5e6df08910e
Julia <julia.rogovaja@gmail.com>
2022-07-13 21:15:22 +0300

    secondHomeWork

---
+++ task_14/Program.cs
@@ -0,0 +1,26 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+var rnd = new Random();
+int digit = rnd.Next(100, 1000);
+System.Console.WriteLine($"Random number 1 from 1...999 is => {digit}");
+
+if (digit % 7 == 0 && digit / 23 == 0) Console.WriteLine($"{digit} кратно ");
+else Console.WriteLine($"{digit} не кратно");
+
+Console.WriteLine("insert number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num % 7 == 0) && (num % 23 == 0)) Console.WriteLine($"{num} кратно ");
+else Console.WriteLine($"{num} не кратно");
+
+
+
+
+
+
+
---
+++ task_16/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+// Console.Write("insert number:  ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int square = num * num;
+// Console.WriteLine($" {num} number square is {square}");
+// double x = Math.Pow(num, 8);
+// Console.WriteLine(x);
+
+var rnd = new Random();
+
+int firstDigit = rnd.Next(1, 1000);
+System.Console.WriteLine($"Random number 1 from 1...999 is => {firstDigit}");
+int secondDigit = rnd.Next(1, 1000);
+System.Console.WriteLine($"Random number 1 from 1...999 is => {secondDigit}");
+
+int square1 = firstDigit * firstDigit;
+int square2 = secondDigit * secondDigit;
+
+if (square1 == secondDigit || square2 == firstDigit)
+    Console.WriteLine($"одно число является квадратом другого.");
+else Console.WriteLine("no");
